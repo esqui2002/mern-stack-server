47e3dd8ae57bfa578a639b1b67ee1c405603e840
Alberto Esquitino <alberto.esqui2002@gmail.com>
2021-03-07 20:46:08 +0100

    Suscribe to newsletter

---
+++ controllers/newsletter.js
@@ -0,0 +1,33 @@
+const Newsletter = require('../models/newsletter');
+
+function suscribeEmail(req, res) {
+	const { email } = req.params;
+
+	const newsletter = new Newsletter();
+
+	if (!email) {
+		res.status(422).send({ code: 422, message: 'El email es obligatorio' });
+	} else {
+		newsletter.email = email.toLowerCase();
+		newsletter.save((err, newsletterStore) => {
+			if (err) {
+				res.status(500).send({ code: 500, message: 'El email ya existe' });
+			} else {
+				if (!newsletterStore) {
+					res.status(400).send({
+						code: 400,
+						message: 'Error la registrar en la newsletter',
+					});
+				} else {
+					res
+						.status(200)
+						.send({ code: 200, message: 'Email registrado correctamente' });
+				}
+			}
+		});
+	}
+}
+
+module.exports = {
+	suscribeEmail,
+};
